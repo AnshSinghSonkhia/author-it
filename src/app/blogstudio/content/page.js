7e4c9cf75cfc88e710cc0f4cb28b88d453d07943
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-02-23 15:58:22 +0530

    Updated: User Content

--- src/app/blogstudio/content/page.js
+++ src/app/blogstudio/content/page.js
@@ -1,8 +1,8 @@
 export default function Content() {
     return (
         <>
-            <div className="text-white">
-                User content will go here
+            <div className="text-white font-bold text-3xl">
+                User Content will be Coming Soon!!
             </div>
 
         </>
